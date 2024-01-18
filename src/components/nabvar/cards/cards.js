977b35fe34f2f1e99ddd8e27f24f7fe2a3c6aeca
Sunnyk16 <kumarsunny1246@gmail.com>
2024-01-19 02:07:10 +0530

    file added

--- src/components/nabvar/cards/cards.js
+++ src/components/nabvar/cards/cards.js
@@ -5,14 +5,29 @@ function Cards() {
   const [fullname , setfullname] =useState('');
   const [password ,setpassword] =useState('');
 
+  const addname=(e)=>{
+    setfullname(e.target.value)}
+
+    const adddetails=()=>{
+
+      const existingdata=JSON.parse(localStorage.getItem('details')|| []);
+      const newdata =[...existingdata ,{'name':fullname , 'password':password}]
+      localStorage.setItem('details' ,JSON.stringify(newdata));
+      setfullname('');
+      setpassword('');
+    }
+
 
   return (
-    <div className='cards'> 
+    <div className='container '>
+      <h1 className='text-center text-capitalize'>Sign in</h1>
+      <div className='card rounded border-1 p-3 shadow-lg'> 
       
-      <input type='text' placeholder='fullname' onClick={(e)=>{localStorage.setItem('name',fullname)}} onChange={(e)=>{setfullname(e.target.value)}}/>
+      <input type='text' className='form-control m-1' placeholder='fullname' onChange={addname} />
       
-      <input type='text' placeholder='password' onClick={(e)=>{localStorage.setItem('password',password) }} onChange={(e)=>{setpassword(e.target.value)}}/>
-      value={password}  
+      <input type='text' className='form-control m-1' placeholder='password'  onChange={(e)=>{setpassword(e.target.value)}}/>
+      <button className='btn btn-danger m-1 w-25 mx-auto' onClick={adddetails}> sign in</button>
+    </div>
     </div>
   )
 }
